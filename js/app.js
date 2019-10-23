60f37048b532b4b57499a160fa322f01e2f36c05
vssobol <vssobol@gmail.com>
2019-10-22 20:45:41 -0700

    dropdown menu has options

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// step 1: add all keywords to drop down menu
+// step 2: make sure keywords dont repeat
+// step 3: filter images by keyword
+
 function Horns(horns) {
     this.url = horns.image_url;
     this.name = horns.title;
@@ -8,7 +12,13 @@ function Horns(horns) {
     this.num = horns.horns;
 }
 
-Horns.all = [];
+Horns.allHorns = [];
+let dropdown = [];
+
+Horns.prototype.options = function() {
+    
+    $('select').append('option')
+}
 
 Horns.prototype.render = function() {
     $('main').append('<div class="clone"></div>');
@@ -20,36 +30,29 @@ Horns.prototype.render = function() {
     hornClone.find('p').text(this.desc);
     hornClone.removeClass('clone');
     hornClone.attr('class', this.keyword);
+    if(dropdown.includes(this.keyword) === false){
+        dropdown.push(this.keyword);
+    }
+    if(dropdown.length === 11){
+        dropdown.forEach(item =>{
+            $('select').append(`<option value="${item}">${item}</option>`);
+
+        })
+    }
 }
 
 Horns.readJson = () => {
     $.get('data/page-1.json', 'json')
         .then(data => {
             data.forEach(item => {
-                Horns.all.push(new Horns(item));
+                Horns.allHorns.push(new Horns(item));
             });
         })
         .then(Horns.loadHorns);
 }
 
 Horns.loadHorns = () => {
-    Horns.all.forEach(horns => horns.render());
+    Horns.allHorns.forEach(horns => horns.render());
 }
 
 $(() => Horns.readJson());
-
-// Dog.readJson = () => {
-//   $.get('./data.json', 'json')
-//     .then(data => {
-//       data.forEach(item => {
-//         Dog.allDogs.push(new Dog(item));
-//       });
-//     })
-//     .then(Dog.loadDogs);
-// };
-
-// Dog.loadDogs = () => {
-//   Dog.allDogs.forEach(dog => dog.render());
-// };
-
-// $(() => Dog.readJson());
